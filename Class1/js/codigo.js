9344b8e946e218a3f46e23b5718fafba1d8852aa
esteban <teba.gonzalez.eg@gmail.com>
2022-05-30 12:52:56 -0400

    ejercicios 1 de Clase 1

---
+++ Class1/js/codigo.js
@@ -0,0 +1,14 @@
+/*ejercio 1 clase 1 sincronico*/
+let nombre;
+let edad;
+alert('Hola Usuario');
+nombre = prompt('cual es tu nombre')
+alert('hola ' + nombre)
+edad = prompt(nombre + 'cual es tu edad ')
+console.log('tu edad ' + edad)
+alert('gracias por tus datos ' + nombre + ' de ' + edad) + 'años';
+document.getElementById('h1ide').innerHTML = "que pasa " + nombre;
+
+/* ejercicio 2 clase 1 sincronico*/
+conteo = prompt('ingrese cualquier palabra ')
+console.log('tu palabra tiene ' + conteo.length + ' caracteres');
